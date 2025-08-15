d5c7f518caf3f1791c2a1211059f9521d170de76
James Kamau <Jimhkamah@gmail.com>
2025-08-15 14:45:23 +0300

    encountering an error when adding the split documents to the vector store

--- pdf_automation.py
+++ pdf_automation.py
@@ -9,36 +9,42 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 # Load HuggingFace embeddings
 embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2") 
+print("HuggingFace embeddings object created:", embeddings)
+
+print(len(load_pdf()))
+print("-------------------------next step---------------------------------")
 
 # initialize the text splitter
 text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000, chunk_overlap=200, add_start_index=True
+    chunk_size=50, chunk_overlap=20, add_start_index=True, separators=[" ", "", "\n\n", "\n", "."]  # Split by spaces or no separator
 )
 all_splits = text_splitter.split_documents(load_pdf())
 
-print(all_splits[:2])  # Print the first split document for verification
+print(len(all_splits)) # Print the number of split documents
+print(all_splits)  # Print the split document for verification
 print("-------------------------next step---------------------------------")
 
 # Initialize the Chroma vector store
-vector_store = Chroma(
-    collection_name="DataScience_collection",
-    embedding_function=embeddings,
-    persist_directory="./chroma_pdf_db",  # Where to save data locally, remove if not necessary
+# vector_store = Chroma(
+#     collection_name="DataScience_collection",
+#     embedding_function=embeddings,
+#     # persist_directory="./chroma_pdf_db",  # Where to save data locally, remove if not necessary
+# )
+
+vector_store = Chroma.from_documents(
+    documents=all_splits,
+    embedding=embeddings
 )
 
 print("chroma initialized")
 print("-------------------------next step---------------------------------")
 
-try:
-    # Add the split documents to the vector store
-    ids = vector_store.add_documents(documents=all_splits)
-    print("Documents added successfully.")
-except Exception as e:
-    print(f"An error occurred while adding documents: {e}")
+# Add the split documents to the vector store
+# ids = vector_store.from_documents(documents=all_splits)
 
 # embedded_query = embeddings.embed_query("who are the publishers?")
 
 print("after embedding query")
 
-results = vector_store.similarity_search("who are the publishers?")
-print(results[0])
+results = vector_store.similarity_search("what are the four class types covering all the basic machine learning functionalities?")
+print(results[0].page_content)
